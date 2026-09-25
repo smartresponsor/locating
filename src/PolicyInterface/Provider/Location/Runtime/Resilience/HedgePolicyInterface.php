71772628bba83b02f1cb8584886a381236a4d85d
Oleksandr Tishchenko <dev@smartresponsor.com>
2026-09-25 09:38:26 -0500

    refactor: move hedge policy to Policy root

--- src/PolicyInterface/Provider/Location/Runtime/Resilience/HedgePolicyInterface.php
+++ src/PolicyInterface/Provider/Location/Runtime/Resilience/HedgePolicyInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Locating\ServiceInterface\Provider\Location\Runtime\Resilience;
+namespace App\Locating\PolicyInterface\Provider\Location\Runtime\Resilience;
 
 interface HedgePolicyInterface
 {
